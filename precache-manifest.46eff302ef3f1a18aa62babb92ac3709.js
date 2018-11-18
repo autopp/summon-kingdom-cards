958f9eaaf0d761e4a6913900e2553c25daa92876
autopp <autopp.inc@gmail.com>
2018-11-18 15:46:07 +0900

    Updates

---
+++ precache-manifest.46eff302ef3f1a18aa62babb92ac3709.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fcd93da3fdf8968b956",
+    "url": "/summon-kingdom-cards/static/js/runtime~main.0fcd93da.js"
+  },
+  {
+    "revision": "f6de5caf0c4c16974255",
+    "url": "/summon-kingdom-cards/static/js/main.f6de5caf.chunk.js"
+  },
+  {
+    "revision": "f20e3543494870a0b0fc",
+    "url": "/summon-kingdom-cards/static/js/1.f20e3543.chunk.js"
+  },
+  {
+    "revision": "f6de5caf0c4c16974255",
+    "url": "/summon-kingdom-cards/static/css/main.f7b0eaa0.chunk.css"
+  },
+  {
+    "revision": "09e7755dc1cce1b3eb85090be9be663c",
+    "url": "/summon-kingdom-cards/index.html"
+  }
+];
